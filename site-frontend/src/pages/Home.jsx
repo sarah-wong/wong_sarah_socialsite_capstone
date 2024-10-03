b2ee5f443c18aa5299a00e548eaa049f6981983f
PerScholarSarah <rmandolin22@gmail.com>
2024-10-03 12:00:25 -0400

    new post form

--- site-frontend/src/pages/Home.jsx
+++ site-frontend/src/pages/Home.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className="page">
+      <h1>Home</h1>
+    </div>
   )
 }
 
--- site-frontend/src/pages/Post.jsx
+++ site-frontend/src/pages/Post.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import {useState, useRef} from 'react'
 
 function Post() {
+  const [formData, setFormData] = useState({
+    title:'',
+    content:'',
+    tags:[]
+  })
+
+  function handleAddTag(evt){
+    evt.preventDefault()
+    if(evt.key === 'Enter')
+    {
+      const newTag = String(evt.target.value).trim()
+      evt.target.value = ''
+      if(!formData.tags.includes(newTag)){
+        setFormData({
+          ...formData,
+          tags:[...formData.tags, newTag]
+        })
+      }
+     
+    }
+  }
+  function removeTag(index){
+    
+  }
+
   return (
-    <div>Post</div>
+    <div className="page">
+      <h1>New Post</h1>
+      <form className="newPostForm">
+          <input type="text" className="titleField" name="title" placeholder="Post Title"/>
+          <textarea className="contentField" name="content" placeholder="Type something..."/>
+          <input type="text" className="addTag" placeholder='add tags'onKeyUp={handleAddTag}/>
+          <div className="tagContainer">
+          {formData.tags.map((tag, idx)=>
+            <span className="tag" key={tag}>
+              #{tag} <button className="inlineBtn delete">X</button>
+            </span>)}
+          </div>
+        </form>
+    </div>
   )
 }
 
