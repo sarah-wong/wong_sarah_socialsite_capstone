2a488fe449cf3b337c4a971512997fbd3e581cf3
PerScholarSarah <rmandolin22@gmail.com>
2024-10-03 14:34:59 -0400

    posting form tested, WORKS

--- site-frontend/src/pages/Post.jsx
+++ site-frontend/src/pages/Post.jsx
@@ -1,4 +1,5 @@
 import {useState, useRef} from 'react'
+import axios from 'axios'
 
 function Post() {
   const [formData, setFormData] = useState({
@@ -7,37 +8,71 @@ function Post() {
     tags:[]
   })
 
+  function handleFormChange(evt){
+    setFormData({
+      ...formData,
+      [evt.target.name]:evt.target.value
+    })
+  }
+  function blockSubmit(evt){
+    if(evt.key === 'Enter'){
+      evt.preventDefault()
+    }
+  }
   function handleAddTag(evt){
-    evt.preventDefault()
+    
     if(evt.key === 'Enter')
     {
-      const newTag = String(evt.target.value).trim()
+      evt.preventDefault()
+      const newTag = evt.target.value
       evt.target.value = ''
-      if(!formData.tags.includes(newTag)){
-        setFormData({
-          ...formData,
-          tags:[...formData.tags, newTag]
-        })
-      }
+
+      const updatedTags = [...formData.tags, newTag]
+      setFormData({
+        ...formData,
+        tags: updatedTags
+      })
+
      
     }
   }
   function removeTag(index){
-    
+    const start = formData.tags.slice(0,index)
+    const end = formData.tags.slice(index+1)
+    setFormData({
+      ...formData,
+      tags: [...start, ...end]
+    })
+  }
+  function handleCreatePost(evt){
+    evt.preventDefault()
+    const token = localStorage.getItem('userAuthToken')
+    const url = `http://localhost:7777/post?token=${token}`
+    const response = axios.post(url, formData)
+    setFormData({
+      title:'',
+      content:'',
+      tags:[]
+    })
+    if(response.status !== 200){
+      console.error('Something when wrong with posting');
+    }
   }
 
   return (
     <div className="page">
       <h1>New Post</h1>
-      <form className="newPostForm">
-          <input type="text" className="titleField" name="title" placeholder="Post Title"/>
-          <textarea className="contentField" name="content" placeholder="Type something..."/>
-          <input type="text" className="addTag" placeholder='add tags'onKeyUp={handleAddTag}/>
+      <form className="newPostForm" onSubmit={handleCreatePost}>
+          <input type="text" className="titleField" name="title" placeholder="Post Title" value={formData.title} onChange={handleFormChange}/>
+          <textarea className="contentField" name="content" placeholder="Type something..." value={formData.content} onChange={handleFormChange}/>
+          <input type="text" placeholder="Add tags... (optional)" onKeyUp={handleAddTag} onKeyDown={blockSubmit} name='ignore'/>
           <div className="tagContainer">
-          {formData.tags.map((tag, idx)=>
-            <span className="tag" key={tag}>
-              #{tag} <button className="inlineBtn delete">X</button>
-            </span>)}
+            {formData.tags.map((tag, idx)=>(
+              <span className="tag" key={tag}>
+                #{tag} <button className="inlineBtn"
+                onClick={()=>removeTag(idx)}>X</button>
+              </span>
+            ))}
           </div>
           <button type="submit">Create Post</button>
         </form>
